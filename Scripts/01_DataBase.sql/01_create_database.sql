df0eec6c489d0ca0974632e6d3e1ab0c8c11a7ff
Viviane de Andrade <157795853+VivianeAndrade1988@users.noreply.github.com>
2026-05-26 11:28:19 -0300

    Update 01_create_database.sql

--- Scripts/01_DataBase.sql/01_create_database.sql
+++ Scripts/01_DataBase.sql/01_create_database.sql
@@ -1,1 +1,20 @@
 
+
+/*=====================================================
+SCRIPT: CRIAÇÃO DO BANCO DATAWAREHOUSE
+=====================================================*/
+
+-- Verifica se banco já existe
+IF DB_ID('DATAWAREHOUSE') IS NOT NULL
+BEGIN
+    DROP DATABASE DATAWAREHOUSE
+END
+GO
+
+-- Cria novo banco
+CREATE DATABASE DATAWAREHOUSE
+GO
+
+-- Usa banco criado
+USE DATAWAREHOUSE
+GO
